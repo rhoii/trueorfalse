dbccbe80aa90c6cf66cf1b12e16fa95eee345bff
rhoii <rhoydelm@gmail.com>
2024-09-16 20:25:04 +0800

    added comment

--- trueorfalse.java
+++ trueorfalse.java
@@ -7,8 +7,8 @@ public class trueorfalse {
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
 
-        boolean isPositive = num > 0;
-        boolean isNegative = num < 0;
+        boolean isPositive = num > 0; //positive
+        boolean isNegative = num < 0; //negative
 
         System.out.println("Is the number positive? " + isPositive);
         System.out.println("Is the number negative? " + isNegative);
